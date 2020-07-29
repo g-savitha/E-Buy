5785658db10fd6abe8810fd99882abb3de5e1b29
Savitha <savitha.gollamudi@gmail.com>
2020-07-29 17:13:40 +0530

    email and password validation using express validators lib

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
+
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signInTemplate = require("../../views/admin/auth/signin");
@@ -9,21 +11,46 @@ router.get("/signup", (req, res) => {
   res.send(signupTemplate({ req }));
 });
 
-router.post("/signup", async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-  const existingUser = await usersRepo.getOneBy({ email: email });
-  if (existingUser) {
-    return res.send("Email in use");
-  }
-  if (password !== passwordConfirmation)
-    return res.send("passwords must match");
-  //create a user
-  const user = await usersRepo.create({ email: email, password: password });
-  //store id of user inside a user cookie
-  req.session.userId = user.id; //req.session is created by cookie session. its an object initially.
+router.post(
+  "/signup",
+  [
+    check("email")
+      .trim()
+      .normalizeEmail()
+      .isEmail()
+      .withMessage("Must be a valid email")
+      .custom(async (email) => {
+        const existingUser = await usersRepo.getOneBy({ email: email });
+        if (existingUser) {
+          throw new Error("Email in Use");
+        }
+      }),
+    check("password")
+      .trim()
+      .isLength({ min: 6, max: 20 })
+      .withMessage("Must be between 6-20 character length"),
+    check("passwordConfirmation")
+      .trim()
+      .isLength({ min: 6, max: 20 })
+      .withMessage("Must be between 6-20 character length")
+      .custom((passwordConfirmation, { req }) => {
+        if (req.body.password !== passwordConfirmation)
+          throw new Error("passwords must match");
+      }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    const { email, password, passwordConfirmation } = req.body;
 
-  res.send("Account created.");
-});
+    //create a user
+    const user = await usersRepo.create({ email: email, password: password });
+    //store id of user inside a user cookie
+    req.session.userId = user.id; //req.session is created by cookie session. its an object initially.
+
+    res.send("Account created.");
+  }
+);
 
 router.get("/signout", (req, res) => {
   //reset the cookie session
