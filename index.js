bae68cffa9db583bb946ceb2ed11201fb228f09c
Savitha <savitha.gollamudi@gmail.com>
2020-08-08 18:24:23 +0530

    shoppingcart-boilerplate

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
 const adminProductsRouter = require("./routes/admin/products");
 const productsRouter = require("./routes/products");
+const cartsRouter = require("./routes/carts");
 const port = 3000 || process.env;
 
 const app = express();
@@ -23,6 +24,7 @@ app.use(
 app.use(authRouter);
 app.use(productsRouter);
 app.use(adminProductsRouter);
+app.use(cartsRouter);
 
 app.listen(port, () => {
   console.log(`listening on port : ${port}`);
---
+++ routes/carts.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const router = express.Router();
+
+//recieve a post req to add an iem to cart
+
+//receive a get req to show all items in cart
+
+//receive a post to delete an item in cart
+module.exports = router;
